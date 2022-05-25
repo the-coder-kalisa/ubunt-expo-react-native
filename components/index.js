1397706d0549e2779a92d50c57a2257793d8cbdf
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-05-25 13:00:05 +0200

    adding flatlist header

--- components/index.js
+++ components/index.js
@@ -2,6 +2,9 @@ import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
 import FocusStatusBar from "./FocusStatusBar";
 import {CircleButton, RectButton} from './Button'
+import DetailsBid from './DetailsBid'
+import DetailsDesc from './DetailsDesc'
+import {SubInfo} from './SubInfo'
 export {
-    HomeHeader, NFTCard, FocusStatusBar, CircleButton, RectButton
+    HomeHeader, NFTCard, FocusStatusBar, CircleButton, RectButton, DetailsDesc, DetailsBid, SubInfo
 }
--- screens/Details.js
+++ screens/Details.js
@@ -1,11 +1,53 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from 'react-native'
+import {COLORS, SIZES, FONTS, assets} from '../constants'
+import {CircleButton, RectButton, SubInfo, FocusStatusBar, DetailsDesc, DetailsBid} from '../components'
+import tw from 'twrnc'
 import React from 'react'
 
-const Details = () => {
-  return (
-    <View>
-      <Text>Details</Text>
+const DetailsHeader = ({data, navigation}) =>{
+    return(
+    <View style={tw`w-full h-[373px]`}>
+        <Image 
+        source={data.image}
+        resizeMode="cover"
+        style={tw`w-full h-full`}
+        />
+        <CircleButton 
+        imgUrl={assets.left}
+        handlePress={()=> navigation.goBack()}
+        left={15}
+        top={10}
+        />
+        <CircleButton 
+        imgUrl={assets.heart}
+        right={15}
+        top={10}
+        />
     </View>
+    )
+}
+const Details = ({route, navigation}) => {
+    const { data } = route.params;
+  return (
+      <SafeAreaView style={tw`android:pt-[${StatusBar.currentHeight}px] h-full`}>
+          <FocusStatusBar barStyle="dark-content" backgroundColor="transparent" translucent={true}/>
+          <View style={tw.style(`w-full absolute bottom-0 py-5 items-center justify-center bg-[rgba(255,255,255,0.5)]`)}>
+              <RectButton minWidth={170} fontSize={SIZES.large}/>
+          </View>
+          <FlatList 
+          data={data.bids}
+          keyExtractor={(item) => item.id}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={tw`pb-5`}
+          renderItem={({item})=> <DetailsBid bid={item} />}
+          ListHeaderComponent={()=>(
+              <React.Fragment>
+                  <DetailsHeader data={data} navigation={navigation}/>
+                  <SubInfo />
+              </React.Fragment>
+          )}
+          />
+      </SafeAreaView>
   )
 }
 
